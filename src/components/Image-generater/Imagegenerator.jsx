dccb6f4436dfb9d8d937e8e830d9b7a704c6ce78
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-02-04 21:32:20 +0530

    added

---
+++ src/components/Image-generater/Imagegenerator.jsx
@@ -0,0 +1,57 @@
+import React, {useRef, useState } from 'react'
+import './Image-generater.css'
+import default_image from '../Assets/default_image.jpg'
+const Imagegenerator = () => {
+
+  const[image_url,setImage_url]=useState("/")
+  let inputRef = useRef(null);
+  const [loading,setloading]=useState(false);
+
+
+  const imageGenerator= async()=>{
+    if(inputRef.current.value===""){
+      return 0;
+    }
+    setloading(true)
+const response=await fetch("https://api.openai.com/v1/images/generations",{
+  method:"POST",
+  headers:{
+    "content-Type":"application/json",
+    Authorization:"Bearer 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",
+    "User-Agent":"Chrome"
+
+  },body:JSON.stringify({
+    prompt:`${inputRef.current.value}`,
+    n:1,
+    size:"512x512",
+  
+  }),
+});
+let data=await response.json();
+let data_array=data.data;
+setImage_url(data_array[0].url);
+setloading(false)
+  }
+
+  return (
+    <div className='image-generator'>
+      <div className="header">Ai image <span>generator</span></div>
+      <div className="img-loading">
+      <div className="image"><img src={image_url==="/"?default_image:image_url} alt=""/></div>
+      <div className="loading">
+    <div className={loading?"loading-bar-full":"loading-bar"}></div>
+    <div className={loading?"loading-text":"display-none"}>loading.....</div>
+   </div>
+    
+    </div>
+    <div className="search-box">
+      <input type="text" ref={inputRef} className='search-input' placeholder='discribe what you want to generate' />
+      <div className="generate-btn" onClick={()=>{imageGenerator()}}>generate</div>
+   
+   
+    </div>
+    </div>
+  )
+}
+
+export default Imagegenerator
